8e69afbbac74795cce999a60b8985e621f4cead8
João Gabriel <joaogabriel.dos.s.pereira@gmail.com>
2025-11-14 00:09:55 -0300

    Front_final

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import Processos from './pages/Processos/Processos'
 import Configuracoes from './pages/Config/Configuracoes'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-
+// === SIDEBAR AJUSTADA ===
 function Sidebar() {
   const linkStyle = {
     color: '#d6ddeb',
@@ -15,35 +15,42 @@ function Sidebar() {
     fontSize: '0.95rem',
     textDecoration: 'none'
   };
-  const linkActive = {
-    color: '#fff',
-    backgroundColor: '#2c5a8a'
-  };
+
   return (
-    <div className="vh-100 p-3" style={{width:240, backgroundColor:'#1a3a5e', color:'#fff', borderRight:'3px solid #2c5a8a'}}>
-      <h4 style={{color:'#fff', fontWeight:600, marginBottom:'30px'}}>CDRView</h4>
+    <div
+      className="vh-100 p-3"
+      style={{
+        width: 240,
+        backgroundColor: '#1a3a5e',
+        color: '#fff',
+        borderRight: '3px solid #2c5a8a'
+      }}
+    >
+
+      <h4 style={{ color: '#fff', fontWeight: 600, marginBottom: '30px' }}>
+        CDRView
+      </h4>
+
       <ul className="nav flex-column">
         <li className="nav-item">
-          <Link className="nav-link" to="/processos" style={linkStyle}>Processos</Link>
+          <Link className="nav-link" to="/processos" style={linkStyle}>
+            Processos
+          </Link>
         </li>
+
         <li className="nav-item">
-          <Link className="nav-link" to="/configuracoes" style={linkStyle}>Configurações</Link>
+          <Link className="nav-link" to="/configuracoes" style={linkStyle}>
+            Configurações
+          </Link>
         </li>
       </ul>
+
     </div>
   );
 }
-}>
-      <h4>CDRView</h4>
-      <ul className="nav flex-column">
-        <li className="nav-item"><Link className="nav-link" to="/processos">Processos</Link></li>
-        <li className="nav-item"><Link className="nav-link" to="/configuracoes">Configurações</Link></li>
-      </ul>
-    </div>
-  )
-}
 
-function App(){
+// === APLICATIVO PRINCIPAL ===
+function App() {
   return (
     <BrowserRouter>
       <div className="d-flex">
@@ -57,7 +64,7 @@ function App(){
         </div>
       </div>
     </BrowserRouter>
-  )
+  );
 }
 
 createRoot(document.getElementById('root')).render(<App />)
